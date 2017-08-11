b8c9aa827f21044e8af2bb1cf1b132e282630d89
Kyle Lange <baronsintrees@gmail.com>
2017-08-11 09:52:01 -0700

    add ability to create data via input that displays.

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -9,11 +9,12 @@ import { Http } from '@angular/http';
 export class PostsComponent implements OnInit {
 
   posts: any[];
+  private url = 'https://jsonplaceholder.typicode.com/posts';
 
   constructor(
-    http: Http
+    private http: Http
   ) {
-    http.get('https://jsonplaceholder.typicode.com/posts')
+    http.get(this.url)
     .subscribe(response => {
       this.posts = response.json();
     });
@@ -22,4 +23,17 @@ export class PostsComponent implements OnInit {
   ngOnInit() {
   }
 
+  createPost(input: HTMLInputElement) {
+    let post = {
+      title: input.value
+    };
+    input.value = '';
+
+    this.http.post(this.url, JSON.stringify(post))
+    .subscribe( response => {
+      post['id'] = response.json().id;
+      this.posts.splice(0, 0, post);
+      console.log(response.json());
+    });
+  }
 }
